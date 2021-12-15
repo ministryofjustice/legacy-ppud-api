1b693a327190ab4e45462f225fa85cb3c66a7a7a
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-12-15 12:07:20 +0000

    Update hmpps.gradle-spring-boot to 3.3.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.15"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.16"
   kotlin("plugin.spring") version "1.5.21"
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
-
-// Force log4j2.version to 2.16 for CVE-2021-45046
-project.extensions.extraProperties["log4j2.version"] = "2.16.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
