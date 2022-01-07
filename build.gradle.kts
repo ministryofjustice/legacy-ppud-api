620e38523a971b28c8d5ca09ea0b8811c1211291
Andy Newton <AndyNewton@users.noreply.github.com>
2022-01-07 10:55:16 +0000

    TECH log4j2.version to 2.17.1 for CVE-2021-44832 (#29)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
-// Force log4j2.version to 2.17 for CVE-2021-45105
-project.extensions.extraProperties["log4j2.version"] = "2.17.0"
+// Force log4j2.version to 2.17.1 for CVE-2021-44832
+project.extensions.extraProperties["log4j2.version"] = "2.17.1"
 // Force logback.version to 1.2.9 (latest stable) for CVE-2021-42550
 project.extensions.extraProperties["logback.version"] = "1.2.9"
 
